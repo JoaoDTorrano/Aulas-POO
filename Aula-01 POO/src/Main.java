5b7bc656ca55496188d297c4f9f9339b8a848e13
João Pedro Torrano Dias <130389414+JoaoDTorrano@users.noreply.github.com>
2026-02-10 14:28:40 -0300

    Add files via upload

---
+++ Aula-01 POO/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z = x + y;
+
+        System.out.println("O valor de z é: " + (x + y));
+
+        System.out.println("Hello World");
+
+    }
+}
