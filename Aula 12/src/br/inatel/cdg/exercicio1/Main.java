7e002b598303de82c48aff02853b9baf440c2211
João Pedro Torrano Dias <130389414+JoaoDTorrano@users.noreply.github.com>
2026-05-28 23:29:13 -0300

    Update Main.java

--- Aula 12/src/br/inatel/cdg/exercicio1/Main.java
+++ Aula 12/src/br/inatel/cdg/exercicio1/Main.java
@@ -1,19 +1,35 @@
 package br.inatel.cdg.exercicio1;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
-        List<Double> listaDeDouble = new ArrayList<>();
-        Random random = new Random();
 
-        for (int i = 0; i < 5; i++) {
-            listaDeDouble.add(random.nextDouble());
-        }
+	public static void main(String[] args) {
 
-        Collections.sort(listaDeDouble, Collections.reverseOrder());
+		List<Double> listaDouble = new ArrayList<Double>();
+		
+		for (int i = 0; i < 5; i++) {
+			listaDouble.add(new Random()
+					.nextDouble()*100);
+		}
+		
+		System.out.println("Antes da Ordenação");
+		
+		for (Double double1 : listaDouble) {
+			System.out.println(double1);
+		}
+		
+		System.out.println("Depois da Ordenação");
+		
+		Collections.sort(listaDouble, Collections.reverseOrder());
+		
+		for (Double double1 : listaDouble) {
+			System.out.println(double1);
+		}
+		
+	}
 
-        for(Double d : listaDeDouble) {
-            System.out.println(d);
-        }
-    }
 }
